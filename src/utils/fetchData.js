f180fd0e1a9bb2008f64f283ba971171b8a2b9a6
Abhianj <104462591+Abhianj@users.noreply.github.com>
2023-01-16 16:51:54 +0530

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '1b1b38d40cmshb974efa2b18d8b2p179c06jsn1fa9f7d40eeb',
+    'X-RapidAPI-Key': '',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -11,7 +11,7 @@ export const youtubeOptions = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+    'X-RapidAPI-Key': '',
   },
 };
 
